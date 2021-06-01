38dbe15e184e72584cd190cdf27b56a405dcd2f4
hasancifciiHasan <hasancifcii@outlook.com>
2021-06-02 01:42:33 +0300

    Day 7 Homework 3

---
+++ src/main/java/kodlamaio/hrms/entities/concretes/Candidate.java
@@ -0,0 +1,34 @@
+package kodlamaio.hrms.entities.concretes;
+
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = false)
+@Data
+@Entity
+@Table(name="candidates")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Candidate extends User{
+
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="identity_number")
+	private String identityNumber;
+	
+	@Column(name="birth_year")
+	private Date birthYear;
+}
